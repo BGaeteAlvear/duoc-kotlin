ac43e95fd64476d0325b7118f98ce2d705f1c38d
unknown <nicousm46@gmail.com>
2018-10-27 20:02:29 -0300

    Diseño recuperar contraseña y diseño de crear usuario

---
+++ app/src/main/java/com/example/n4c0/kotlinapp/activities/ForgotPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.n4c0.kotlinapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.n4c0.kotlinapp.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
--- app/src/main/java/com/example/n4c0/kotlinapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/n4c0/kotlinapp/activities/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.n4c0.kotlinapp
+package com.example.n4c0.kotlinapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.n4c0.kotlinapp.R
 
 class LoginActivity : AppCompatActivity() {
 
